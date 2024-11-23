f876ad449bb214af0a6a4c8ee90f92a5ab3e39d9
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-11-23 10:39:06 +0700

    btvn_ss4_bai1

---
+++ ss4-bai1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("moi ban nhap vao so nguyen ");
+	scanf("%d",&a);
+	if (a>0){
+		printf("day la so duong");
+	} else {
+		printf ("day la so am");
+	}
+	
+	return 0;
+}
